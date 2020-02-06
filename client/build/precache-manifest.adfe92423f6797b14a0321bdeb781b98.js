278c872250257f81978fde8da3cdfd97151d8afd
oscar <worell-oscar@hotmail.com.br>
2020-02-06 15:47:39 -0200

    teste

---
+++ client/build/precache-manifest.adfe92423f6797b14a0321bdeb781b98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e0b2bd56b4801adee1a90273e9b5b2e",
+    "url": "/reactProject/index.html"
+  },
+  {
+    "revision": "fae7f9b60b87a765a0e6",
+    "url": "/reactProject/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a259e9cc7fbfec11f748",
+    "url": "/reactProject/static/css/main.9226dc8a.chunk.css"
+  },
+  {
+    "revision": "fae7f9b60b87a765a0e6",
+    "url": "/reactProject/static/js/2.a93e09e2.chunk.js"
+  },
+  {
+    "revision": "93631d7b5470ae920b6e694679e6e339",
+    "url": "/reactProject/static/js/2.a93e09e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a259e9cc7fbfec11f748",
+    "url": "/reactProject/static/js/main.81d2eebf.chunk.js"
+  },
+  {
+    "revision": "ae6219cbe69db121d003",
+    "url": "/reactProject/static/js/runtime-main.4192f927.js"
+  },
+  {
+    "revision": "2908c619494ec0c1fc68d038d0dda923",
+    "url": "/reactProject/static/media/ImgExemplo1.2908c619.jpg"
+  }
+]);
